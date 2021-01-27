f54e39b798b8014d306a692524aebb0eae3c856e
miyatomo <t.miya19890131@gmail.com>
2021-01-27 09:19:22 +0900

    develop

--- src/components/Contents/Globals/Header/index.tsx
+++ src/components/Contents/Globals/Header/index.tsx
@@ -3,11 +3,18 @@ import { Menu } from "@material-ui/icons";
 import TextSearch from "../../Inputs/TextSearch";
 import { Grid } from "@material-ui/core";
 
-const Header: React.FC = ({}) => {
+
+type Props = {
+  actions: {
+    clickMenu: any;
+  }
+}
+
+const Header: React.FC<Props> = ({actions}) => {
   return (
     <Grid container  style={{height: '100%', backgroundColor: '#E4E4E4'}}>
       <Grid item xs={2} style={{display: 'table', height: '100%'}}>
-        <Menu fontSize="large" style={{margin: '16px', display: 'table-cell', verticalAlign: 'middle', cursor: 'pointer'}} />
+        <Menu fontSize="large" style={{margin: '16px', display: 'table-cell', verticalAlign: 'middle', cursor: 'pointer'}} onClick={actions.clickMenu}/>
       </Grid>
       <Grid item xs={8}>
         <TextSearch label={"検索"} placeholder={"製品名を入力してください"} fullWidth={true} />
