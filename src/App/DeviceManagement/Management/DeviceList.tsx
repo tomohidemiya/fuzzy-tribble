fc058cdd125b3fe4b4b2d73af0b2a4f116af97ac
miyatomo <t.miya19890131@gmail.com>
2021-02-06 00:42:14 +0900

    master

---
+++ src/App/DeviceManagement/Management/DeviceList.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+type Props = {}
+
+const DeviceList: React.FC<Props> = () => {
+  return (
+    <></>
+  );
+}
+
+export default DeviceList;
--- src/App/DeviceManagement/Management/index.tsx
+++ src/App/DeviceManagement/Management/index.tsx
@@ -1,11 +1,18 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
+import DeviceList from './DeviceList';
 
 type Props = {}
 
-const DeviceManagement: React.FC<Props> = () => {
+const Management: React.FC<Props> = () => {
   return (
-    <></>
+    <div style={{height: '100%'}}>
+      <div style={{padding: '16px'}}>
+        <Typography variant="h6" color="inherit">機器管理</Typography>
+        <DeviceList />
+      </div>
+    </div>
   );
 }
 
-export default DeviceManagement;
+export default Management;
